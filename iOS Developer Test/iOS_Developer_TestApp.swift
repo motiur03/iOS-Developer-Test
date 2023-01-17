a6acdccd5885896361f6d911e81497d4fbd2f8f1
Motiur Rahaman <suza.ruet@gmail.com>
2023-01-16 19:29:40 -0600

    Initial Commit

---
+++ iOS Developer Test/iOS_Developer_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Developer_TestApp.swift
+//  iOS Developer Test
+//
+//  Created by Motiur Rahaman on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Developer_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
